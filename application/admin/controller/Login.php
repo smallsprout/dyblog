67cd3453b15f36b76afd1a4d10d424a25bdbbf01
xiaodouya <374140546@qq.com>
2018-01-10 19:58:45 +0800

    my first blog

---
+++ application/admin/controller/Login.php
@@ -0,0 +1,25 @@
+<?php
+namespace app\Admin\controller;
+use think\Controller;
+use app\admin\model\Admin;
+class Index extends Controller
+{
+    public function index()
+    {
+        if(request()->isPost()){
+            $admin=new Admin();
+            $data=input('post.');
+            $num=$admin->login($data);
+            if($num==3){
+                 $this->success('登录成功了','Index/index');   
+            }elseif($num==4){
+                $this->error('验证码错误');
+            }
+            else{
+                $this->error('用户名或者密码错误');
+            }
+
+        }
+        return $this->fetch('login');
+    }
+}
